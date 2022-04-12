a0e38abe6452dd9dd214d8b292ece0bdd82923d4
RemusPlays <97199766+RemusPlays@users.noreply.github.com>
2022-04-12 16:22:48 -0700

    Added menus and screenshots
    - Added title screen and button
    - Added instruction screen and button
    - Added back button for main game screen
    - Took screenshots to prepare for submission

--- OOP Project/Assets/Scripts/GameManager.cs
+++ OOP Project/Assets/Scripts/GameManager.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -66,6 +65,11 @@ public class GameManager : MonoBehaviour
         GameObject[] destroyBalls;
         destroyBalls = GameObject.FindGameObjectsWithTag("balltag");
         foreach (GameObject bowlingBall in destroyBalls) Destroy(bowlingBall);
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(1);
     }
 
 }
